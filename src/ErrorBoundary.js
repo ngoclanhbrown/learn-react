113d93cde6386217bfe8a12265cae0468eb08d48
ngoclanhbrown <tnlanh99@gmail.com>
2021-11-10 12:08:58 +0700

    Add ErrorBoundary component

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          Something went wrong. <Link to="/">Click here</Link> to go back to the
+          home page.
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
